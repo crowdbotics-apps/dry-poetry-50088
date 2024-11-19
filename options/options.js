0cbf373e58a294c4b679a40c3bdac2294274beeb
Crowdbotics <team@crowdbotics.com>
2024-11-19 12:39:55 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "dry-poetry-50088",
+  url: "https://dry-poetry-50088.botics.co",
+  api: "https://dry-poetry-50088.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
